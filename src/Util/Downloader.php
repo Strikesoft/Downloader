512a3cbb318a2ec7895b957fb6f35038d12f71e1
Johann-S <johann.servoire@gmail.com>
2017-01-01 23:45:28 +0100

    Allow to download files from Mega

--- src/Util/Downloader.php
+++ src/Util/Downloader.php
@@ -22,6 +22,9 @@ class Downloader {
 
     public function download(Request $request) {
         $url = $request->request->get('url', null);
+        if (strpos($url, 'https://mega.nz/') === 0) {
+            return $this->megaDownload($url);
+        }
         $checkFile = $this->checkFile($url);
         if ($checkFile['error']) {
             return array('error' => $checkFile['message']);
@@ -34,6 +37,47 @@ class Downloader {
 
         // Security about fake images
         if ($checkFile['isImage']) {
+            list($width, $height) = getimagesize($pathFile);
+            if ($width === 0 && $height === 0) {
+                unlink($pathFile);
+                return array('error' => 'Not a valid image');
+            }
+        }
+        return array(
+            'downloadLink' => $this->downloadUrl . $filename,
+            'filename'     => $filename
+        );
+    }
+
+    private function megaDownload($url) {
+        $mega = new Mega();
+        $fileInfo = $mega->public_file_info_from_link($url);
+        $filename = null;
+        if (isset($fileInfo['at']['n']) && !empty($fileInfo['at']['n'])) {
+            $filename = $fileInfo['at']['n'];
+        }
+        else {
+            return array(
+                'error' => true,
+                'message' => 'No file name'
+            );
+        }
+        $ext = strtolower(substr(strrchr($filename, '.'), 1));
+        if (!in_array($ext, $this->allowedFiles) && !in_array($ext, $this->allowedImages)) {
+            return array(
+                'error' => true,
+                'message' => 'file not allowed'
+            );
+        }
+
+        $fileInfo = array_merge($fileInfo, Mega::parse_link($url));
+        $pathFile = $this->pathDownload . $filename;
+        $fp = fopen($pathFile, 'wb');
+        $mega->public_file_download($fileInfo['ph'], $fileInfo['key'], $fp);
+        fclose($fp);
+
+        // Security about fake images
+        if (in_array($ext, $this->allowedImages)) {
             list($width, $height) = getimagesize($pathFile);
             if ($width === 0 && $height === 0) {
                 unlink($pathFile);
